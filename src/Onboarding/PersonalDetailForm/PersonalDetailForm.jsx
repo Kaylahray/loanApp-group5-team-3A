47f4dac4bcb667606391bde1565a434f9290e17d
Miriam Asadu <miriam.odenyi@gmail.com>
2023-05-23 15:21:07 +0100

    imported form control

--- src/Onboarding/PersonalDetailForm/PersonalDetailForm.jsx
+++ src/Onboarding/PersonalDetailForm/PersonalDetailForm.jsx
@@ -2,15 +2,45 @@ import styles from "./personalDetailForm.module.scss";
 import FormBox from "../../Component/FormBox/FormBox";
 import ImageBox from "../../Component/ImageBox/ImageBox";
 import Image from "../../Component/Image/Image";
+import { useState } from "react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+} from "@chakra-ui/react";
 
 const PersonalDetailForm = () => {
+  const [input, setInput] = useState(" ");
+  const handleInputChange = (e) => setInput(e.target.value);
+  const isError = input === "";
   return (
     <div className={styles.main}>
+      <h1>
+        <img src="./component/img.svg" alt="" />
+      </h1>
       <ImageBox>
         <Image />
       </ImageBox>
       <FormBox>
-        <div>formmmmmmmmmmmmm</div>
+        <section>
+          <div className={styles.logo}></div>
+          <FormControl isInvalid={isError}>
+            <FormLabel className="label">First name (required)</FormLabel>
+            <Input placeholder="e.g. Oluwatofunmi" type="text" />
+            <FormErrorMessage>First name is required</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={isError}>
+            <FormLabel>Last name (required)</FormLabel>
+            <Input placeholder="e.g. Idowu" type="text" />
+            <FormErrorMessage>Last name is required</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={isError}>
+            <FormLabel>Role (required)</FormLabel>
+            <Input placeholder="e.g. Bank manager" type="text" />
+            <FormErrorMessage>Role is required</FormErrorMessage>
+          </FormControl>
+        </section>
       </FormBox>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import PersonalDetailForm from "./Onboarding/PersonalDetailForm/PersonalDetailForm.jsx";
+import Signup from "./Onboarding/SignUpPage/Signup.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <App />
+        <PersonalDetailForm />
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
