577df3c970061d36d01228dcf3bcc690e48163db
Sandy <peacesandy04@gmail.com>
2023-05-24 03:31:07 -0700

    made changes

--- src/Onboarding/ResetPasswordSent/ResetPasswordSent.jsx
+++ src/Onboarding/ResetPasswordSent/ResetPasswordSent.jsx
@@ -7,7 +7,7 @@ const ResetPasswordSent = () => {
       <h1 className={styles.h1text}>Reset password</h1>
       <div>
         <p className={styles.ptext}>We just sent a password reset link to   <a href="#">peacesandy04@gmail.com</a>
-        <br/> Click on the link to 
+        <br/> Click on the link to complete
         complete password reset</p>
 
         <p className={styles.ptext2}>Please know that the link will expire in 4 hours</p>
