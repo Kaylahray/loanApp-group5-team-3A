a984e90f087d6c5a9f83e5b1f8ef46f766b49acf
chioma <chiomachristopher91@gmail.com>
2023-06-03 04:27:03 -0700

    changes

--- src/Pages/Chats/MessageCenter/MessageCenterFooter.jsx
+++ src/Pages/Chats/MessageCenter/MessageCenterFooter.jsx
@@ -1,20 +1,21 @@
+// eslint-disable-next-line react/prop-types
 export default function MessageCenterFooter({ sendMessage }) {
-    const onHandleInput = (event) => {
-        if (event.key == "Enter") {
-            sendMessage(event.target.value);
-            event.target.value = "";
-        }
+  const onHandleInput = (event) => {
+    if (event.key == "Enter") {
+      sendMessage(event.target.value);
+      event.target.value = "";
     }
-    return (
-      <div className="bottom">
-        <img src="/images/attachment.svg" alt="" />
-        <img src="/images/emoji.svg" alt="" />
-        <input
-          className=" input placeholder text"
-          type="text"
-          placeholder="Type message"
-          onKeyDown={onHandleInput}
-        />
-      </div>
-    );
+  };
+  return (
+    <div className="bottom">
+      <img src="/images/attachment.svg" alt="" />
+      <img src="/images/emoji.svg" alt="" />
+      <input
+        className=" input placeholder text"
+        type="text"
+        placeholder="Type message"
+        onKeyDown={onHandleInput}
+      />
+    </div>
+  );
 }
--- src/Pages/Models/Models.jsx
+++ src/Pages/Models/Models.jsx
@@ -1,6 +1,6 @@
-import { BiSearch } from "react-icons/bi";
-import styles from "./Models.module.scss";
-import Logo from "./ModelIcons/SLogo.svg";
+// import { BiSearch } from "react-icons/bi";
+// import styles from "./Models.module.scss";
+// import Logo from "./ModelIcons/SLogo.svg";
 import ModelTop from "./ModelTop";
 
 const Models = () => {
