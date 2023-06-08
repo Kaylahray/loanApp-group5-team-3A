ef69e3bffb54813f7835557c97e8e0b18de6498e
chioma <chiomachristopher91@gmail.com>
2023-06-08 05:41:45 -0700

    updating from 618a8892fee76a61dbdf43a318f85e70a0ffd311

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,10 @@
-import { useState } from "react";
-import "./App.scss";
-import { Routes, Route } from "react-router-dom";
-
-import Container from "./Component/Container/Container";
-import { Context } from "./Context";
-import Layout from "./Component/LayoutComponent/Layout";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import Application from "./Pages/Applications/UserApplications/Application";
-import Teams from "./Pages/Teams/Teams";
-import Chats from "./Pages/Chats/Chats";
-import Settings from "./Pages/Settings/Settings";
-import Faq from "./Pages/Faq/Faq";
-import Models from "./Pages/Models/Models";
-import Logout from "./Pages/Logout/Logout";
-
-import CustomerProfile from "./Pages/CustomerProfile/CustomerPro";
-
-
-import SignUp from "./OnboardingPages/SignUp";
-import PersonalDetails from "./OnboardingPages/PersonalDetails";
-import LandingPage from "./OnboardingPages/LandingPage";
-import AddTeam from "./OnboardingPages/AddTeam";
-import UploadLogo from "./OnboardingPages/UploadLogo";
-import LoginForm from "./OnboardingPages/LoginForm";
-import LoginFormTwo from "./OnboardingPages/LoginFormTwo";
-import ForgotPassword from "./OnboardingPages/ForgotPassword";
-import ResetPassword from "./OnboardingPages/ResetPassword";
-import ResetPasswordSent from "./OnboardingPages/ResetPasswordSent";
-import PasswordResetSuccess from "./OnboardingPages/PasswordResetSuccess";
-import VerifyEmail from "./OnboardingPages/VerifyEmail";
-import VerificationSuccess from "./OnboardingPages/VerificationSuccess";
+import "./App.css";
 
 function App() {
-  const [theme, setTheme] = useState("light");
-  const [show, setShow] = useState(false);
-  const visiblePassword = () => setShow(!show);
-
-  // const changeTheme = () => {
-  //   setTheme(theme === "light" ? "dark" : "light");
-  // };
-
   return (
-    <Context.Provider value={{ visiblePassword, show, setTheme }}>
-      <div className={`${theme} application`}>
-        <Container>
-          {/* <button className="btn" onClick={changeTheme}>
-            theme
-          </button> */}
-
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/signup" element={<SignUp />} />
-
-            <Route path="/pdetails" element={<PersonalDetails />} />
-            <Route path="/forgotPassword" element={<ForgotPassword />} />
-            <Route path="/resetPassword" element={<ResetPassword />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/logTwo" element={<LoginFormTwo />} />
-            <Route path="/add" element={<AddTeam />} />
-            <Route path="/up" element={<UploadLogo />} />
-            <Route path="/resetPasswordSent" element={<ResetPasswordSent />} />
-            <Route
-              path="/passwordResetSuccess"
-              element={<PasswordResetSuccess />}
-            />
-            <Route path="/verify" element={<VerifyEmail />} />
-            <Route path="/verifySuccess" element={<VerificationSuccess />} />
-            <Route path="/layout" element={<Layout />}>
-              <Route path="/layout/" element={<Dashboard />} />
-              <Route path="/layout/dashboard" element={<Dashboard />} />
-
-              <Route path="/layout/application" element={<Application />}>
-                {/* <Route
-                  path="/layout/application/customerprofile"
-                  element={<CustomerProfile />}
-                /> */}
-              </Route>
-              <Route path="/layout/teams" element={<Teams />} />
-              <Route path="/layout/models" element={<Models />} />
-
-              <Route path="/layout/settings" element={<Settings />} />
-              <Route path="/layout/chats" element={<Chats />} />
-              <Route path="/layout/faq" element={<Faq />} />
-              <Route path="/layout/logout" element={<Logout />} />
-            </Route>
-            <Route path="/models" element={<Models />} />
-
-          </Routes>
-        </Container>
-      </div>
-    </Context.Provider>
+    <>
+      <h1>TEAM 3A</h1>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
-import * as React from "react";
-// import { ChakraProvider } from "@chakra-ui/react";
-import * as ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
-import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
